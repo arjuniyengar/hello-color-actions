0c43f4fad58c1be4f4ac2ed590c3fa991a4f5b93
arjuniyengar <arjun.iyengar@gmail.com>
2024-01-29 20:49:40 -0700

    Changed the display string - arjun

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from arjuns home in Salt Lake City, UT!";
+const hello = () => "Hello World from arjuns home in Utah";
 console.log(hello());
 exports.hello = hello;
