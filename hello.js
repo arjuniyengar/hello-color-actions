6c3f5f3125cf4576ec473cb78fee8aff3716d5e4
arjuniyengar <arjun.iyengar@gmail.com>
2024-01-29 19:36:46 -0700

    arjun made changes to the file

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from arjuns home in Salt Lake City, UT!";
 console.log(hello());
 exports.hello = hello;
