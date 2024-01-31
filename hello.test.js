387bac456c668b4d39c2d27c55f7b7820953143a
arjuniyengar <arjun.iyengar@gmail.com>
2024-01-30 17:07:25 -0700

    attemp 4 after fixing aws region

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from arjuns home in Sandy Utah");
+    expect(hello.hello()).toEqual("Hello World from arjuns home in Utah");
   });
 });
